61ee687fa61626916dcdb067e36219b8dd546458
a.nguyen525i@gmail.com <a.nguyen525i@gmail.com>
2019-11-18 01:10:35 -0700

    seed users and events

---
+++ scripts/userSeed.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const db = require("../models");
+
+
+mongoose.connect(
+    process.env.MONGODB_URI || 
+    "mongodb://localhost/bestLife"
+);
+
+const userSeed = [
+    {
+        name: "Antwan DuChez",
+        email: "antwan@yomomashouse.com",
+        totalEventPoints: 200, 
+        totalFoodPoints: 400, 
+        totalWorkoutPoints: 100,
+
+    },
+    {
+        name: "Jillian Anderson",
+        email: "jillfunk@yahoo.com",
+        totalEventPoints: 150, 
+        totalFoodPoints: 225, 
+        totalWorkoutPoints: 350,
+    },
+    {
+        name: "Jack Sparrow",
+        email: "antwan@yomomashouse.com",
+        totalEventPoints: 200, 
+        totalFoodPoints: 400, 
+        totalWorkoutPoints: 100,
+    },
+    {
+        name: "Dwayne Johnson",
+        email: "therock@badass.com",
+        totalEventPoints: 575, 
+        totalFoodPoints: 450, 
+        totalWorkoutPoints: 1000,
+    }
+];
+
+db.User
+    .remove({})
+    .then(() => db.User.collection.insertMany(userSeed))
+    .then(data => {
+        console.log(data.result.n + " records inserted!");
+        process.exit(0);
+    })
+    .catch(err => {
+        console.error(err);
+        process.exit(1);
+    });
